f89a32970365caf379863cdb44271768b2dfb312
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2022-07-17 10:55:20 +0100

    Update Custom-Search.py

--- Custom-Search.py
+++ Custom-Search.py
@@ -67,38 +67,36 @@ if st.button('Start Search'):
     #content_area = st.text_input('Specify regex to find div classes within your content areas:')
     
     ### The following loop opens each url 1 by 1 and saves the text in "div class="layout__region layout__region--content container" to text_column_data
-    for url in urls:
-        print("Loading URLs...")
-        st.progress(url)
-        try:   
-            fd = urlopen(url)
-            f = io.BytesIO(fd.read()) 
+    with st.spinner("Loading URLs..."):
+        for url in urls:
+            try:   
+                fd = urlopen(url)
+                f = io.BytesIO(fd.read()) 
 
-            soup = BeautifulSoup(f, 'html.parser')
-            content_div_html = soup.find("div", {"class": re.compile(f"{content_area}")})
-            content_div_text = content_div_html.get_text().lower()
-            url_column_data.append(url)
-            text_column_data.append(content_div_text)
-        except:
-            pass
+                soup = BeautifulSoup(f, 'html.parser')
+                content_div_html = soup.find("div", {"class": re.compile(f"{content_area}")})
+                content_div_text = content_div_html.get_text().lower()
+                url_column_data.append(url)
+                text_column_data.append(content_div_text)
+            except:
+                pass
 
     ### The following adds the url data to our main output dataframe
-    output["url"] = url_column_data
+        output["url"] = url_column_data
 
     ### The following loop opens each word 1 by 1
-    for word in words:
-        print("Searching for words...")
-        st.progress(word)
-        search_column_data = []
-        try:
-            ### The following loop within a loop opens the text_column_data list 1 by 1 and counts each word 1 by 1
-            for text in text_column_data:      
-                word_count_in_text = text.count(word)
-                search_column_data.append(word_count_in_text)
-        except:
-            pass
-       ### The following adds the count data for each word to our output dataframe
-        output[word] = search_column_data
+    with st.spinner("Searching for words..."):
+        for word in words:
+            search_column_data = []
+            try:
+                ### The following loop within a loop opens the text_column_data list 1 by 1 and counts each word 1 by 1
+                for text in text_column_data:      
+                    word_count_in_text = text.count(word)
+                    search_column_data.append(word_count_in_text)
+            except:
+                pass
+            ### The following adds the count data for each word to our output dataframe
+            output[word] = search_column_data
 
 #print(output)
     st.write("""
